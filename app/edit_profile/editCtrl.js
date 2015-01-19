3850e672261e5077a69e54edaa528c64dc4b1156
Samantha Stallings <samantha.stallings1@gmail.com>
2015-01-19 10:06:18 -0800

    added editForm model to view

--- app/edit_profile/editCtrl.js
+++ app/edit_profile/editCtrl.js
@@ -4,5 +4,13 @@ angular
   .controller('EditController', ['user', 'userService', function (user, userService) {
 
     var vm = this;
-    vm.edit = userService.user;
+    vm.editForm = {};
+
+    vm.edit = function () {
+      console.log(vm.editForm);
+      EditFactory.edit(vm.editForm).then(function () {
+        vm.editForm = {};
+        $state.go('profile');
+      });
+    };
   }]);
--- app/edit_profile/editFactories.js
+++ app/edit_profile/editFactories.js
@@ -1,8 +1,28 @@
 angular
   .module('homey')
 
-  .factory('editFactory', ['$http', '$q', 'editService', function ($http, $q, editService) {
+  .factory('editFactory', ['$http', '$q', function ($http, $q) {
+
     return {
 
+      edit: function(editForm) {
+        var d = $q.defer();
+
+        $http.put('http://localhost:3000/api/edit', {
+          email: editForm.email,
+          first_name: editForm.first_name,
+          last_name: editForm.last_name,
+          age: editForm.age,
+          phone: editForm.phone
+        })
+        .success(function (response) {
+          console.log('success', response);
+          d.resolve(response.user);
+        }).error(function (response) {
+          console.log('error', response);
+          d.reject(response.error);
+        });
+        return d.promise;
+      }
     }
   }]);
