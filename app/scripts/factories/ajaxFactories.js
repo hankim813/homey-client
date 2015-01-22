cfeebe48144513ddf35f24df8a96113b432b2d86
Han Kim <hankim813@gmail.com>
2015-01-22 11:49:28 -0800

    extract ajax requests to factory

---
+++ app/scripts/factories/ajaxFactories.js
@@ -0,0 +1,30 @@
+angular
+	.module('homey')
+
+	.factory('ajaxFactory', ['$http', '$q', function ($http, $q) {
+		return {
+			request: function (uri, type) {
+				var d = $q.defer();
+
+				switch (type) {
+					case 'get':
+						(function () {
+							$http.get(uri).success(function (response) {d.resolve(response);}).error(function (response) {d.reject(response.error);})
+						})();
+						break;
+					case 'post':
+						(function () {
+							$http.post(uri).success(function (response) {d.resolve(response);}).error(function (response) {d.reject(response.error);})
+						})();
+						break;
+					case 'put':
+						(function () {
+							$http.put(uri).success(function (response) {d.resolve(response);}).error(function (response) {d.reject(response.error);})
+						})();
+						break;
+				}
+
+				return d.promise;
+			}
+		}
+	}]);
