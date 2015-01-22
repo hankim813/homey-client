c6552f162d208d84602b03dfcf08ac76fa737b10
Han Kim <hankim813@gmail.com>
2015-01-22 08:13:29 -0800

    extract fb js sdk to fbfactories

--- app/auth/fbFactories.js
+++ app/auth/fbFactories.js
@@ -4,6 +4,24 @@ angular.
 	.factory('fbFactory', ['LoginFactory', '$state', function (LoginFactory, $state) {
 		return {
 
+			initialize: function () {
+		    window.fbAsyncInit = function() {
+		      FB.init({
+		        appId      : '836908086372942',
+		        xfbml      : true,
+		        version    : 'v2.2'
+		      });
+		    };
+
+		    (function(d, s, id){
+		       var js, fjs = d.getElementsByTagName(s)[0];
+		       if (d.getElementById(id)) {return;}
+		       js = d.createElement(s); js.id = id;
+		       js.src = "//connect.facebook.net/en_US/sdk.js";
+		       fjs.parentNode.insertBefore(js, fjs);
+		     }(document, 'script', 'facebook-jssdk'));
+			},
+
 			fbLogin: function () {
 				FB.login(function (response) {
 					if (response.status === 'connected') {
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -54,23 +54,6 @@ angular
 
   .run(['$rootScope', '$location', 'AuthFactory', function ($rootScope, $location, AuthFactory) {
 
-    // FB Login Initialize
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId      : '836908086372942',
-        xfbml      : true,
-        version    : 'v2.2'
-      });
-    };
-
-    (function(d, s, id){
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) {return;}
-       js = d.createElement(s); js.id = id;
-       js.src = "//connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-     }(document, 'script', 'facebook-jssdk'));
-
     AuthFactory.check();
 
     if (AuthFactory.isLogged) {
