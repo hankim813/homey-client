20f954d0814a69b43599a7a9f539542ccaddf4d9
Han Kim <hankim813@gmail.com>
2015-03-03 12:28:40 -0800

    finish layout for services page with new assets

--- app/scripts/controllers/servicesCtrl.js
+++ app/scripts/controllers/servicesCtrl.js
@@ -7,10 +7,15 @@ angular
 		vm.list = [
 			{title: 'Home Cleaning', class: 'cleaning', icon: 'cleaning-icon.png', state: 'homeCleaning'},
 			{title: 'Car Wash', class: 'carwash', icon: 'car-wash.png', state: 'carWash'},
-			{title: 'Plumbing', class: 'plumbing', icon: 'plumbing-icon.png', state: 'plumbing'},
-			{title: 'Electrical', class: 'electric', icon: 'electric-icon.png', state: 'electric'},
-			{title: 'Painting', class: 'painting', icon: 'painting-icon.png', state: 'painting'},
+			{title: 'Office Cleaning', class: 'office', icon: 'office-icon.png', state: 'office'},
 			{title: 'Personal Chefs', class: 'chefs', icon: 'cooking-icon.png', state: 'chef'},
+			{title: 'Painting', class: 'painting', icon: 'painting-icon.png', state: 'painting'},
+			{title: 'Electrical', class: 'electric', icon: 'electric-icon.png', state: 'electric'},
+			{title: 'Chauffeur', class: 'driver', icon: 'driver-icon.png', state: 'driver'},
+			{title: 'Plumbing', class: 'plumbing', icon: 'plumbing-icon.png', state: 'plumbing'},
 			{title: 'Security Guards', class: 'security', icon: 'security-icon.png', state: 'security'},
+			{title: 'Gardening', class: 'gardening', icon: 'gardening-icon.png', state: 'gardening'},
+			{title: 'Mechanical', class: 'mechanical', icon: 'mechanical-icon.png', state: 'mechanical'},
+			{title: 'Contractor Job', class: 'contractor', icon: 'contractor-icon.png', state: 'contractor'},
 		];
 	}]);
